73d39af05e751528e87c486c20303c941f9848b4
Dusan Jovanovic <jovanovic.ra144.2019@uns.ac.rs>
2022-10-26 21:38:20 +0200

    Add hospital description, import fonts, styling

--- src/app/modules/feedback/model/feedback.model.ts
+++ src/app/modules/feedback/model/feedback.model.ts
@@ -1,36 +1,22 @@
 export class Feedback implements FeedbackInterface{
 
-    public id: number;
-    public patientId: number;
+    public patientFullName: string;
     public text: string;
     public rating: number;
     public creationDate: Date;
-    public isAnonymous: boolean;
-    public isPublic: boolean;
-    public feedbackStatus: FeedbackStatus;
 
     constructor(feedbackCfg: FeedbackInterface)
     {
-        this.id = feedbackCfg.id;
-        this.patientId = feedbackCfg.patientId;
+        this.patientFullName = feedbackCfg.patientFullName;
         this.text = feedbackCfg.text;
         this.rating = feedbackCfg.rating;
         this.creationDate = feedbackCfg.creationDate;
-        this.isAnonymous = feedbackCfg.isAnonymous;
-        this.isPublic = feedbackCfg.isPublic;
-        this.feedbackStatus = feedbackCfg.feedbackStatus;
     }
 }
 
 interface FeedbackInterface{
-    id: number;
-    patientId: number;
+    patientFullName: string;
     text: string;
     rating: number;
     creationDate: Date;
-    isAnonymous: boolean;
-    isPublic: boolean;
-    feedbackStatus: FeedbackStatus;
 }
-
-export enum FeedbackStatus { OnHold, Approved, Denied }
